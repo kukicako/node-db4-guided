a0d195a1a244ea07dbdad11a77cb885ba657a637
Muamer Kukic <kukicako@gmail.com>
2019-11-07 10:54:43 -0800

    commit

---
+++ data/migrations/20191107104419_name.js
@@ -0,0 +1,50 @@
+exports.up = function(knex) {
+    return knex.schema
+      .createTable('species', tbl => {
+        // the type of the Primary Key is: integer without negative values, also called unsigned
+        tbl.increments();
+        tbl.string('name', 255).notNullable();
+      })
+      .createTable('animals', tbl => {
+        tbl.increments();
+        tbl.string('name', 255).notNullable();
+        // define our Foreign Key
+        tbl
+          .integer('species_id')
+          .unsigned()
+          .references('id')
+          .inTable('species')
+          .onDelete('RESTRICT') // about deleting the record from the primary key table. Could be 'RESTRICT', 'NO ACTION', 'SET NULL'
+          .onUpdate('CASCADE'); // about changing the value of the primary key table.
+        // we have bears and a few animals that are bears.
+      })
+      .createTable('zoos', tbl => {
+        tbl.increments();
+        
+        tbl.string('name', 255).notNullable();
+        tbl.string('address', 255).notNullable();
+      })
+      .createTable('animal_zoos', tbl => {
+        tbl.increments();
+        
+        tbl.integer('zoo_id')
+        .unsigned()
+          .references('id')
+          .inTable('zoos')
+          .onDelete('RESTRICT') 
+          .onUpdate('CASCADE'); 
+        tbl.integer('animal_id')
+        .unsigned()
+          .references('id')
+          .inTable('animals')
+          .onDelete('RESTRICT') 
+          .onUpdate('CASCADE'); 
+      })
+
+  };
+  exports.down = function(knex) {
+      return knex.schema
+      .dropTableIfExists('animal_zoos')
+      .dropTableIfExists('zoos')
+  };
+  // knex ... command not found: knex -> npx knex ... or install knex globally with npm i -g knex
